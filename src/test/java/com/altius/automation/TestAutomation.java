092caed852c08b0c33e39bb3e0984b8f4b80ab40
rajivrathinam <rajiv.rathinam4@gmail.com>
2018-12-27 13:02:53 +0530

    initial commit

---
+++ src/test/java/com/altius/automation/TestAutomation.java
@@ -0,0 +1,24 @@
+package com.altius.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestAutomation {
+
+WebDriver driver;
+	
+	@BeforeTest
+	public void beforeExecution(){
+		System.setProperty("webdriver.chrome.driver", "../automationTemplate/src/main/resources/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void sampleTest(){
+		driver.get("http://www.google.com");
+		driver.close();
+	}
+	
+}
